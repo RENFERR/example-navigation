e9098252c7c8152783369e44a9fbe3c95d27c435
Kirill <maenkov.k@tk23.ru>
2023-04-07 16:37:09 +0400

    small fixes, removed back stack calls

--- app/src/main/java/com/example/nav/ui/elements/drawer/Drawer.kt
+++ app/src/main/java/com/example/nav/ui/elements/drawer/Drawer.kt
@@ -1,11 +1,11 @@
-package com.example.nav.drawer
+package com.example.nav.ui.elements.drawer
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,23 +13,23 @@ import androidx.compose.ui.unit.dp
 import com.example.nav.screens.destinations.ListScreenDestination
 import com.example.nav.screens.destinations.LoginScreenDestination
 import com.example.nav.screens.destinations.UserScreenDestination
+import com.example.nav.ui.elements.drawer.DrawerItem.Companion.toRoute
 import com.example.nav.ui.theme.NavigationExampleTheme
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kotlinx.coroutines.launch
 
 @Composable
 fun Drawer(
     state: DrawerState,
+    currentPage: DrawerItem,
     navigator: DestinationsNavigator,
     content: @Composable () -> Unit
 ) {
     val drawerItems = listOf(
-        DrawerItem.Items,
+        DrawerItem.List,
         DrawerItem.User,
         DrawerItem.Logout
     )
-    var currentPage: DrawerItem by rememberSaveable {
-        mutableStateOf(DrawerItem.Items)
-    }
     Drawer(
         drawerItems = drawerItems,
         state = state,
@@ -37,13 +37,37 @@ fun Drawer(
         currentPage = currentPage,
         onItemClick = { item ->
             when (item) {
-                DrawerItem.Items -> navigator.navigate(ListScreenDestination)
-                DrawerItem.Logout -> navigator.navigate(LoginScreenDestination)
-                DrawerItem.User -> navigator.navigate(UserScreenDestination.invoke("My username"))
+                DrawerItem.List -> navigator.list(currentRoute = currentPage.toRoute())
+                DrawerItem.Logout -> navigator.logout(currentRoute = currentPage.toRoute())
+                DrawerItem.User ->
+                    navigator.user(currentRoute = currentPage.toRoute(), argument = "MyUsername")
             }
-            currentPage = item
         }
     )
+}
+
+private fun DestinationsNavigator.list(currentRoute: String) {
+    navigate(direction = ListScreenDestination) {
+        popUpTo(route = currentRoute) {
+            inclusive = true
+        }
+    }
+}
+
+private fun DestinationsNavigator.logout(currentRoute: String) {
+    navigate(direction = LoginScreenDestination) {
+        popUpTo(route = currentRoute) {
+            inclusive = true
+        }
+    }
+}
+
+private fun DestinationsNavigator.user(currentRoute: String, argument: String) {
+    navigate(direction = UserScreenDestination.invoke(username = argument)) {
+        popUpTo(route = currentRoute) {
+            inclusive = true
+        }
+    }
 }
 
 @Composable
@@ -54,6 +78,7 @@ private fun Drawer(
     onItemClick: (DrawerItem) -> Unit,
     content: @Composable () -> Unit
 ) {
+    val scope = rememberCoroutineScope()
     ModalNavigationDrawer(
         drawerState = state,
         drawerContent = {
@@ -71,7 +96,10 @@ private fun Drawer(
                                 item = item,
                                 selected = currentPage == item,
                                 onClick = {
-                                    if (currentPage != item) onItemClick.invoke(item)
+                                    if (currentPage != item) {
+                                        if (state.isOpen) scope.launch { state.close() }
+                                        onItemClick.invoke(item)
+                                    }
                                 }
                             )
                         }
@@ -88,7 +116,7 @@ private fun Drawer(
 private fun DrawerPreview() {
     val state = rememberDrawerState(initialValue = DrawerValue.Open)
     val drawerItems = listOf(
-        DrawerItem.Items,
+        DrawerItem.List,
         DrawerItem.User,
         DrawerItem.Logout
     )
@@ -97,7 +125,7 @@ private fun DrawerPreview() {
         state = state,
         content = {},
         onItemClick = {},
-        currentPage = DrawerItem.Items
+        currentPage = DrawerItem.List
     )
 }
 
